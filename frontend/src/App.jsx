974248234fbe8f892e2258739a9c8c5791927a7c
Rohit3124 <rohitkumawat4493@gmail.com>
2024-12-24 15:42:08 +0530

    create pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 export default function App() {
-  return <div className="text-3xl text-red-500">App</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/Sign-in",
+      element: <SignIn />,
+    },
+    {
+      path: "/Sign-up",
+      element: <SignUp />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
