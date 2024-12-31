72c5113cc1d83ce75db8fb7d8a77243a43e3415f
Rohit3124 <rohitkumawat4493@gmail.com>
2024-12-31 19:49:21 +0530

    create user modal

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/blog")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
