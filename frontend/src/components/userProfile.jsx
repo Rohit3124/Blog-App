21ad2556ae78fbadde206ac27c08a2eebe3f2a55
Rohit3124 <rohitkumawat4493@gmail.com>
2025-01-11 19:37:17 +0530

    add user image in profile

--- frontend/src/components/userProfile.jsx
+++ frontend/src/components/userProfile.jsx
@@ -1,12 +1,43 @@
+import { useState, useRef, useEffect } from "react";
+
 const UserProfile = () => {
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef();
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(e.target.files[0]);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (imageFile) uploadImage();
+  }, [imageFile]);
+  const uploadImage = async () => {
+    console.log("uploading image...");
+  };
   return (
     <div className="w-96 mx-auto">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form action="" className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center cursor-pointer">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="w-32 h-32 self-center cursor-pointer"
+          onClick={() => filePickerRef.current.click()}
+        >
           <img
             alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+            src={
+              imageFileUrl ||
+              "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+            }
             className="rounded-full w-full h-full object-cover border-8"
           />
         </div>
